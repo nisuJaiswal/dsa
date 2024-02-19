bf30b1825ad13ad574149dfb82b7af36ee9ecf15
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-02-19 09:29:53 +0530

    Reverse Using Swapping

--- Arrays/Reverse.js
+++ Arrays/Reverse.js
@@ -1,6 +1,18 @@
 const arr = [12,14,54,77,43,1]
 
-const reverseArr = (arr) => {
+const reverseArrStatic = (arr) => {
     return arr.reverse()
 }
-console.log(reverseArr(arr))
+const reverseUsingSwap = (arr,start,end) => {
+    // console.log(arr)
+    while(start < end) {
+        let temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start++
+        end--
+    }
+    return arr
+}
+console.log(reverseArrStatic(arr))
+console.log(reverseUsingSwap(arr,0,arr.length -1 ))
