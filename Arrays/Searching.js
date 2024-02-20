9ab7cf28ca6045cb0c85c303f7699f159eea869a
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-02-20 19:52:45 +0530

    Searching...

---
+++ Arrays/Searching.js
@@ -0,0 +1,14 @@
+const usingFor = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) return i;
+  }
+
+  return -1;
+};
+
+const usingInBuilt = (nums, target) => {
+  return nums.findIndex((n) => n === target);
+};
+const nums = [0, 54, 1, 2, 3, 4, 7];
+console.log(usingFor(nums, 10));
+console.log(usingInBuilt(nums, 10));
