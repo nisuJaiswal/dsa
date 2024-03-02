4bbf63ad5abafcb0009e9feb95a8ec0d6a20c685
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-03-02 11:13:04 +0530

    Longest Common Prefix

---
+++ Strings/longestCommonPrefix.js
@@ -0,0 +1,15 @@
+const longestPrefix = (str) => {
+  let prefix = "";
+
+  str.sort();
+  let left = str[0],
+    right = str[str.length - 1];
+
+  for (let i = 0; i < Math.min(left.length, right.length); i++) {
+    if (left[i] !== right[i]) return prefix;
+    prefix += left[i];
+  }
+
+  return prefix;
+};
+console.log(longestPrefix(["flower", "flow", "flight"]));
