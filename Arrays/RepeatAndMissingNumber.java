dc3023258554d1f05070da8fca30c79518df683f
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-06-03 21:42:30 +0530

    RepeatMissingNumebr and StockBuySell java

---
+++ Arrays/RepeatAndMissingNumber.java
@@ -0,0 +1,39 @@
+package Arrays;
+
+public class RepeatAndMissingNumber {
+    public static int[] findRepeated(final int[] A) {
+        int duplicate = -1;
+        int missing = -1;
+
+        for (int i = 0; i < A.length; i++) {
+            int index = Math.abs(A[i]) - 1;
+            if (A[index] < 0) {
+                duplicate = Math.abs(A[i]);
+            } else {
+                A[index] = -A[index];
+            }
+            // System.out.println("I: " + i + " Duplicate: " + duplicate);
+        }
+        System.out.println("\nAfter finding duplicates: ");
+        for (int el : A)
+            System.out.println(el);
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        return new int[] { duplicate, missing };
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 1, 2, 5, 3 };
+        int[] result = findRepeated(arr);
+
+        System.out.println("\n Here's the result: ");
+        for (int el : result)
+            System.out.println(el + " ");
+    }
+}
---
+++ Arrays/StockBuySell.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class StockBuySell {
+    public static int predictBuy(int[] prices) {
+        int buyVal = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (buyVal > prices[i])
+                buyVal = prices[i];
+
+            maxProfit = Math.max(maxProfit, Math.abs(buyVal - prices[i]));
+        }
+
+        return maxProfit;
+
+    }
+
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println("\nMaximum Profit will be: " + predictBuy(prices));
+    }
+}
