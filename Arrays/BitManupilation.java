967c578aa7e2031013ea4d614adeb1dca47d880c
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-06-07 19:27:39 +0530

    BitManipulation java

---
+++ Arrays/BitManupilation.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class BitManupilation {
+    public static void usingSimpleApproach(int a, int b) {
+        int size = Math.abs(b - a) + 1;
+        int[] arr = new int[size];
+
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0 || i % 5 == 0) {
+                arr[i - a] = 1;
+            }
+        }
+
+        System.out.println("\n Here's the result : ");
+        for (int el : arr)
+            System.out.println(el);
+        for (int i = a; i <= b; i++) {
+            if (arr[i - a] == 1)
+                System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        usingSimpleApproach(2, 10);
+    }
+}
