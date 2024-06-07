1475c898ba8d43dfbe6364cd6c2b868d48556f15
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-06-07 20:16:47 +0530

    CheckParantheses java

---
+++ Strings/CheckParantheses.java
@@ -0,0 +1,29 @@
+package Strings;
+
+import java.util.Stack;
+
+public class CheckParantheses {
+    public static boolean checkParantheses(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else {
+                if (stack.isEmpty() || stack.pop() != c)
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println(checkParantheses("()[{}"));
+
+    }
+}
