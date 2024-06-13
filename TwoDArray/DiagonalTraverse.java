a7d2944a6878ccb172d7094fad632ada97326b27
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-06-13 11:03:48 +0530

    DiagonalTraverse java

---
+++ TwoDArray/DiagonalTraverse.java
@@ -0,0 +1,49 @@
+package TwoDArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiagonalTraverse {
+
+    public static List<Integer> getDiagonal(int N, int[][] A) {
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int x = 0;
+            int y = i;
+
+            while (x < N && y >= 0) {
+                res.add(A[x][y]);
+                x++;
+                y--;
+            }
+
+        }
+
+        for (int i = 1; i < N; i++) {
+            int x = i;
+            int y = N - 1;
+            while (x < N && y >= 0) {
+                res.add(A[x][y]);
+                x++;
+                y--;
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+        System.out.println();
+        List<Integer> response = getDiagonal(3, arr);
+
+        for (int i : response)
+            System.out.print(i + " ");
+    }
+}
