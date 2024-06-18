3f44ecfa8e41d954bb9ee7fa338b0ec477ef6d2c
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-06-18 11:14:03 +0530

    StackUsingQueue java

--- StackQueues/TwoStacks.java
+++ StackQueues/TwoStacks.java
@@ -8,53 +8,55 @@ public class TwoStacks {
     TwoStacks(int size) {
         this.size = size;
         arr = new int[size];
-        top1 = (size / 2) - 1;
-        top2 = size / 2;
+        top1 = -1; // Initialize top1 to -1
+        top2 = size; // Initialize top2 to size
     }
 
     void pushFirst(int el) {
-        if (top1 > 0) {
-            top1--;
+        if (top1 < top2 - 1) {
+            top1++;
             arr[top1] = el;
         } else {
-            System.out.println("First Stack Overflow " + top1);
+            System.out.println("First Stack Overflow");
         }
-        return;
     }
 
     void pushSecond(int el) {
-        if (top2 < size - 1) {
-            top2++;
+        if (top2 > top1 + 1) {
+            top2--;
             arr[top2] = el;
         } else {
             System.out.println("Second Stack Overflow");
         }
-
-        return;
     }
 
     int popFirst() {
-        if (top1 <= size / 2) {
+        if (top1 >= 0) {
             int temp = arr[top1];
-            top1++;
+            top1--;
             return temp;
         } else {
             System.out.println("First Stack Underflow");
-            System.exit(0);
+            return -1; // Return -1 to indicate underflow
         }
-        return -1;
     }
 
     int popSecond() {
         if (top2 < size) {
             int temp = arr[top2];
-            top2--;
+            top2++;
             return temp;
         } else {
             System.out.println("Second Stack Underflow");
-            System.exit(0);
+            return -1; // Return -1 to indicate underflow
         }
-        return -1;
     }
 
+    public static void main(String[] args) {
+        TwoStacks ts = new TwoStacks(10);
+        ts.pushFirst(5);
+        ts.pushSecond(10);
+        System.out.println(ts.popFirst());
+        System.out.println(ts.popSecond());
+    }
 }
