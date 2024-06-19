7a0eab0db8a88232d612c09e6074c4ce8d0964ba
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-06-19 22:30:12 +0530

    Intution of LenOfLongestValidSubstring java

--- StackQueues/LenOfLongestValidSubstring.java
+++ StackQueues/LenOfLongestValidSubstring.java
@@ -1,6 +1,16 @@
 package StackQueues;
 
 import java.util.Stack;
+// Intution:
+// 1. Initialize the integer stack and push the -1 to it and initiliazie the result with 0
+// 2. Traverse through the string.
+// 3. if the character is ( then simply push it into the stack
+// 4. If the character is ) then
+// 4.1 if the stack is non empty then pop the first element from the stack
+// 4.2 if the stack is non empty then
+// 4.2.1 Update the result by taking the diff between current index (i) and the top of the stack (Math.max(result, i - stack.peek()))
+// 4.2.2 Push the index of the i into the stack
+// 5. Return the value of result
 
 public class LenOfLongestValidSubstring {
     public static int lenOfLongest(String str) {
