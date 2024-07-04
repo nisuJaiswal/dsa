b4dee1df2ce74f857a2f33e44b7fcbcc118772fa
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-07-04 10:19:27 +0530

    MinDiffBetweenMaxMinInThreeMoves java

---
+++ Arrays/MInDiffBetweenMinMaxInThreeMoves.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MInDiffBetweenMinMaxInThreeMoves {
+    public static int giveDiff(int[] arr) {
+        if (arr.length <= 4)
+            return 0;
+
+        Arrays.sort(arr);
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < 4; i++) {
+            System.out.println(res + " " + arr[arr.length - 4 + i] + " " + arr[i]);
+            res = Math.min(res, arr[arr.length - 4 + i] - arr[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 5, 0, 10, 14 };
+        // 0 1 5 10 14
+        System.out.println();
+        System.out.println(giveDiff(arr));
+    }
+}
