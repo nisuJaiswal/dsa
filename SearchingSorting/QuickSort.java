c3fe750d81c8a3813b6fbcafa7e0db0540ebe865
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-08-03 11:15:55 +0530

    QuickSort java

---
+++ SearchingSorting/QuickSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int i = start - 1;
+        int j = end + 1;
+
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+            if (i >= j)
+                return j;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+        }
+
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int pivotIndex = partition(arr, start, end);
+            quickSort(arr, start, pivotIndex);
+            quickSort(arr, pivotIndex + 1, end);
+        }
+    }
+
+    public static void main(String[] ar) {
+        int[] arr = { 4, 2, 3, 5, 1 };
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
