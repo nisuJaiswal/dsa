502a0f084877d751b8a544f7563a9e0ae5536553
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-08-03 11:42:06 +0530

    InsertionSort java

---
+++ SearchingSorting/InsertionSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int curr = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && curr < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[++j] = curr;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 1, 2, 4 };
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
